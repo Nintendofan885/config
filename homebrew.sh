f4db886bacea99eb17715bcea002b903d7636922
Nathaniel Schaaf <nschaaf@wikimedia.org>
2016-02-11 10:10:19 -0600

    Add homebrew

---
+++ homebrew.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if ! hash brew 2>/dev/null; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+
+
+brew install macvim
+brew install git
+brew install wget
+brew install node
+brew install postgresql
+brew install php56
+brew install php56-pdo-pgsql
+brew install python3
+brew install gnupg
+
+brew tap caskroom/cask
+
+brew cask install google-chrome
+brew cask install shiftit
