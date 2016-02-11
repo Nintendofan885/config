8381a7b816416415262e420cd1a4f449b420e638
Nathaniel Schaaf <nschaaf@wikimedia.org>
2016-02-11 11:27:10 -0600

    Remove excess whitespace

--- homebrew.sh
+++ homebrew.sh
@@ -4,8 +4,6 @@ if ! hash brew 2>/dev/null; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-
-
 brew install macvim
 brew install git
 brew install wget
