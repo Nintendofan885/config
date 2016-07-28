e0238f9c538d3cc56d212455a8e882172657d2f7
Nathaniel Schaaf <nschaaf@wikimedia.org>
2016-07-28 18:53:27 +0200

    Add more preferences for Dock and Terminal

--- utils/osx_settings.sh
+++ utils/osx_settings.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Dock
+/usr/libexec/PlistBuddy -c "Set :tilesize 46" $HOME/Library/Preferences/com.apple.dock.plist
+/usr/libexec/PlistBuddy -c "Set :autohide 1" $HOME/Library/Preferences/com.apple.dock.plist
+/usr/libexec/PlistBuddy -c "Set :magnification 1" $HOME/Library/Preferences/com.apple.dock.plist
+
 # Show hidden files in Finder
 defaults write com.apple.finder AppleShowAllFiles YES
 
@@ -7,6 +12,15 @@ defaults write com.apple.finder AppleShowAllFiles YES
 /usr/libexec/PlistBuddy -c "Set :'Default Window Settings' Pro" $HOME/Library/Preferences/com.apple.Terminal.plist
 /usr/libexec/PlistBuddy -c "Set :'Startup Window Settings' Pro" $HOME/Library/Preferences/com.apple.Terminal.plist
 /usr/libexec/PlistBuddy -c "Set :'Window Settings':Pro:shellExitAction 0" $HOME/Library/Preferences/com.apple.Terminal.plist
+/usr/libexec/PlistBuddy -c "Set :'Window Settings':Pro:rowCount 40" $HOME/Library/Preferences/com.apple.Terminal.plist
+/usr/libexec/PlistBuddy -c "Set :'Window Settings':Pro:columnCount 150" $HOME/Library/Preferences/com.apple.Terminal.plist
+/usr/libexec/PlistBuddy -c "Set :'Window Settings':Pro:ShowActiveProcessArgumentsInTitle 1" $HOME/Library/Preferences/com.apple.Terminal.plist
+/usr/libexec/PlistBuddy -c "Set :'Window Settings':Pro:ShowRepresentedURLPathInTitle 1" $HOME/Library/Preferences/com.apple.Terminal.plist
+/usr/libexec/PlistBuddy -c "Set :'Window Settings':Pro:ShowTTYNameInTitle 0" $HOME/Library/Preferences/com.apple.Terminal.plist
+/usr/libexec/PlistBuddy -c "Set :'Window Settings':Pro:ShowCommandKeyInTitle 0" $HOME/Library/Preferences/com.apple.Terminal.plist
+/usr/libexec/PlistBuddy -c "Set :'Window Settings':Pro:ShowDimensionsInTitle 0" $HOME/Library/Preferences/com.apple.Terminal.plist
+/usr/libexec/PlistBuddy -c "Set :'Window Settings':Pro:ShowActiveProcessInTitle 1" $HOME/Library/Preferences/com.apple.Terminal.plist
+/usr/libexec/PlistBuddy -c "Set :'Window Settings':Pro:ShowShellCommandInTitle 0" $HOME/Library/Preferences/com.apple.Terminal.plist
 
 # Add login items
 for item in Caffeine Flux 'Google Drive' 'Google Play Music Desktop Player' ShiftIt; do
